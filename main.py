a6f577bd56216ba99a838a70a0b535ddfaacd720
Gabriel Souza <rorato91@gmail.com>
2022-06-22 09:31:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -23,10 +23,22 @@ frame_corpo.grid(row=1, column=0)
 botao_1 = Button(frame_corpo, text='C', width=11, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 botao_1.place(x=0, y=0)
 
-botao_2 = Button(frame_corpo, text='%', width=5, height=2, bg=cor5, fg=cor2)
-botao_2.place(x=90, y=0)
+botao_2 = Button(frame_corpo, text='%', width=5, height=2, bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+botao_2.place(x=118, y=0)
 
-botao_3 = Button(frame_corpo, text='/', width=5, height=2, bg=cor5, fg=cor2)
+botao_3 = Button(frame_corpo, text='/', width=5, height=2, bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 botao_3.place(x=177, y=0)
+
+botao_4 = Button(frame_corpo, text='7', width=5, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+botao_4.place(x=0, y=52)
+
+botao_5 = Button(frame_corpo, text='8', width=5, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+botao_5.place(x=59, y=52)
+
+botao_6 = Button(frame_corpo, text='9', width=5, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+botao_6.place(x=118, y=52)
+
+botao_7 = Button(frame_corpo, text='*', width=5, height=2, bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+botao_7.place(x=177, y=52)
 
 janela.mainloop()
